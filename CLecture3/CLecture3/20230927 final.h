df7f54799ea219d0ec5c5278340f2fd68cf69943
7erz <leesncat@naver.com>
2023-10-02 19:49:12 +0900

    231002 선형 컨테이너 Vector

---
+++ CLecture3/CLecture3/20230927 final.h
@@ -0,0 +1,33 @@
+#pragma once
+#include<iostream>
+using namespace std;
+
+#pragma region final
+//하위클래스에서 더 이상 재정의할 수 없도록 선언하는 기능
+//계층을 많이 쓸때 사용하게 됨
+
+class Hero {
+	virtual void BasicSkill() {};
+	virtual void MagicSkill() {};
+
+};
+
+class Crusader : public Hero {
+	void BasicSkill() {};
+	void MagicSkill() final {};
+};
+
+class Fighter : public Crusader {
+	void BasicSkill() {};
+	//void MagicSkill() {};
+};
+
+#pragma endregion
+
+
+
+int main() {
+
+
+	return 0;
+}
